f4e48218c431c8cd30e0d089509e3b64d4e69ab8
yanjiepeng <yan930806@gmail.com>
2019-07-09 14:21:37 +0800

    异步对话框和Toast提示DEMO

--- lib/pages/DialogDemo.dart
+++ lib/pages/DialogDemo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class DialogDemo extends StatefulWidget {
   @override
@@ -6,6 +7,102 @@ class DialogDemo extends StatefulWidget {
 }
 
 class _DialogDemoState extends State<DialogDemo> {
+  //异步创建ALERTdialog
+  _alertDialog() async {
+    var result = await showDialog(
+        barrierDismissible: false, //表示点击灰色背景的时候是否消失弹出框
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("提示信息!"),
+            content: Text("您确定要删除吗?"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("取消"),
+                onPressed: () {
+                  print("取消");
+                  Navigator.pop(context, 'Cancle');
+                },
+              ),
+              FlatButton(
+                child: Text("确定"),
+                onPressed: () {
+                  print("确定");
+                  Navigator.pop(context, "Ok");
+                },
+              )
+            ],
+          );
+        });
+
+    print(result);
+  }
+
+  _simpleDialog() async {
+    var dialog = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text('选择'),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text('option1'),
+                onPressed: () {
+                  print('option1');
+                },
+              ),
+              Divider(),
+              SimpleDialogOption(
+                child: Text('option2'),
+                onPressed: () {
+                  print('option2');
+                },
+              ),
+              Divider(),
+              SimpleDialogOption(
+                child: Text('option3'),
+                onPressed: () {
+                  print('option3');
+                },
+              ),
+              FlatButton(onPressed: () {}, child: Text('确定'))
+            ],
+          );
+        });
+    print(dialog);
+  }
+
+  _modelBottomSheet() async {
+    var result = await showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 220,
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.share),
+                  title: Text('分享A'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.share),
+                  title: Text('分享B'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(Icons.share),
+                  title: Text('分享C'),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          );
+        });
+
+    print(result);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,76 +153,138 @@ class _DialogDemoState extends State<DialogDemo> {
               disabledColor: Colors.lightBlueAccent,
             );
           }),
-          RaisedButton(
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return SimpleDialog(
-                      title: Text('选择'),
-                      children: <Widget>[
-                        SimpleDialogOption(
-                          child: Text('option1'),
-                          onPressed: () {},
-                        ),
-                        SimpleDialogOption(
-                          child: Text('option1'),
-                          onPressed: () {},
-                        ),
-                      ],
-                    );
-                  });
-            },
-            child: Text('simpleDialog'),
-            color: Colors.blue,
-            highlightColor: Colors.lightBlueAccent,
-            disabledColor: Colors.lightBlueAccent,
-          ),
-          RaisedButton(
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('标题'),
-                      content: SingleChildScrollView(
-                        child: ListBody(
-
+          Wrap(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return SimpleDialog(
+                          title: Text('选择'),
                           children: <Widget>[
-
-                            Text('内容1'),
-                            Text('内容2')
-
+                            SimpleDialogOption(
+                              child: Text('option1'),
+                              onPressed: () {},
+                            ),
+                            SimpleDialogOption(
+                              child: Text('option1'),
+                              onPressed: () {},
+                            ),
                           ],
-
-
-                        ),
-                      ),
-
-                      actions: <Widget>[
-
-                        FlatButton(onPressed: (){
-
-                          Navigator.of(context).pop();
-
-                        }, child: Text('确定')),
-
-                        FlatButton(onPressed: (){
-
-                          Navigator.of(context).pop();
-
-                        }, child: Text('取消')),
-                      ],
-
-
-                    );
-                  });
-            },
-            child: Text('AlertDialog'),
-            color: Colors.blue,
-            highlightColor: Colors.lightBlueAccent,
-            disabledColor: Colors.lightBlueAccent,
+                        );
+                      });
+                },
+                child: Text('simpleDialog'),
+                color: Colors.blue,
+                highlightColor: Colors.lightBlueAccent,
+                disabledColor: Colors.lightBlueAccent,
+              ),
+              RaisedButton(
+                onPressed: this._simpleDialog,
+                child: Text('异步simpledialog'),
+              )
+            ],
           ),
+          Wrap(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('标题'),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: <Widget>[Text('内容1'), Text('内容2')],
+                            ),
+                          ),
+                          actions: <Widget>[
+                            FlatButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('确定')),
+                            FlatButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('取消')),
+                          ],
+                        );
+                      });
+                },
+                child: Text('AlertDialog'),
+                color: Colors.blue,
+                highlightColor: Colors.lightBlueAccent,
+                disabledColor: Colors.lightBlueAccent,
+              ),
+              RaisedButton(
+                child: Text('异步_alertDialog'),
+                onPressed: _alertDialog,
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Wrap(
+            children: <Widget>[
+              Builder(builder: (BuildContext context) {
+                return RaisedButton(
+                  child: Text('BottomSheet'),
+                  onPressed: () {
+                    showBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return Container(
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Column(
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text('title1'),
+                                    leading: Icon(Icons.chat),
+                                  ),
+                                  ListTile(
+                                    title: Text('title2'),
+                                    leading: Icon(Icons.help),
+                                  ),
+                                  ListTile(
+                                    title: Text('title3'),
+                                    leading: Icon(Icons.search),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  },
+                );
+              }),
+              RaisedButton(
+                onPressed: this._modelBottomSheet,
+                child: Text('异步bottomsheet'),
+              )
+            ],
+          ),
+          Text(
+              'showModalBottomSheet与BottomSheet的区别是 BottomSheet充满屏幕，ModalBottomSheet半屏'),
+          SizedBox(
+            height: 10,
+          ),
+          RaisedButton(
+            onPressed: () {
+              Fluttertoast.showToast(
+                  msg: '提示信息',
+                  gravity: ToastGravity.BOTTOM,
+                  timeInSecForIos: 10,
+                  textColor: Colors.white,
+                  backgroundColor: Colors.black);
+            },
+            child: Text('Toast'),
+          )
         ],
       ),
     );
